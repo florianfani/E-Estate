2123c9236e2770d3b8cb503577eee2c11e95e2b0
florianfani <93375399+florianfani@users.noreply.github.com>
2023-11-02 20:02:43 +0100

    update listing functionallity done

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -215,7 +215,9 @@ export default function Profile() {
 
                   <div className='flex flex-col items-center'>
                     <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700'>Delete</button>
-                    <button className='text-green-700'>Edit</button>
+                    <Link to={`/update-listing/${listing._id}`}>
+                      <button className='text-green-700'>Edit</button>
+                    </Link>
                   </div>
                 </div>
               ))}
